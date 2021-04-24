598801d610ba592757b4affb8e940251d056d5b5
jopejoso <jjopek0@gmail.com>
2021-04-24 16:14:46 +0200

    Changed build script

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
-[ -d "snekapp" ] && rm -rf snekapp
-mkdir snekapp
-cp snek.py snekapp/__main__.py
-python -m pip install -r requirements.txt --target snekapp
-rm -rf snekapp/*.dist-info/
-python -m zipapp snekapp
+[ -d "builds/" ] && rm -rf builds/
+mkdir -p builds/snekapp
+cp snek.py builds/snekapp/__main__.py
+python -m pip install -r requirements.txt --target builds/snekapp
+rm -rf builds/snekapp/*.dist-info/
+python -m zipapp -o builds/snek.pyz builds/snekapp
+echo "color 7 & python snek.pyz & exit" > builds/snek.bat
